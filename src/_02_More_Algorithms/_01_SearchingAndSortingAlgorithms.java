a69bed19eb65c007dd0f12e80ea2ada4664e0768
Dong Zou <jiwenzou@gmail.com>
2019-04-13 15:50:06 -0700

    Sorting Doubles

--- src/_02_More_Algorithms/_01_SearchingAndSortingAlgorithms.java
+++ src/_02_More_Algorithms/_01_SearchingAndSortingAlgorithms.java
@@ -1,6 +1,8 @@
 package _02_More_Algorithms;
 
-import static org.junit.Assert.*;
+
+
+import static org.junit.Assert.assertEquals;
 
 import java.util.Arrays;
 import java.util.List;
@@ -35,7 +37,6 @@ public class _01_SearchingAndSortingAlgorithms {
 		List<Double> peeps = Arrays.asList(new Double[] { 5.7, 6.2, 3.4, 6.2, 5.0, 5.5, 4.7, 6.2, 3.4, 4.2, 5.0, 4.5, 5.1, 6.6 });
 		assertEquals(6.6, Algorithms.findTallest(peeps), 0.0);
 	}
-
 	/* 4. Create a method that will find the longest word. */
 	@Test
 	public void testFindMaxiumumString() throws Exception {
@@ -67,9 +68,9 @@ public class _01_SearchingAndSortingAlgorithms {
 	public void testSort() throws Exception {
 		List<Double> results = Arrays.asList(new Double[] { 60.3, 60.2, 80.4, 67.2, 95.0, 85.5, 40.7, 68.2, 38.4, 94.2, 85.0, 84.5, 50.1,
 				66.6 });
-		assertEquals(38.4, Algorithms.sortScores(results).get(0), 0.0);
-		assertEquals(40.7, Algorithms.sortScores(results).get(1), 0.0);
-		assertEquals(95.0, Algorithms.sortScores(results).get(13), 0.0);
+		assertEquals(Algorithms.sortScores(results).get(0), 38.4, 0.0);
+		assertEquals(Algorithms.sortScores(results).get(1), 40.7, 0.0);
+		assertEquals( Algorithms.sortScores(results).get(13),95.0, 0.0);
 	}
 
 	/* 8. Sort the DNA sequences by length from shortest to longest. */
